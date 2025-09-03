866ed21fd36321fdfc1cae11dd76e5d3df4f845a
Angel Padilla <56833527+kratosmemes@users.noreply.github.com>
2025-09-02 20:56:58 -0600

    Se agregan componentes para home y lista de usuarios y compoennte notfound

---
+++ src/app/Screens/users-home-component/users-home-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-users-home-component',
+  imports: [RouterOutlet],
+  templateUrl: './users-home-component.html',
+  styleUrl: './users-home-component.scss'
+})
+export class UsersHomeComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from "@angular/router";
 import { FormsComponent } from "./Screens/forms-component/forms-component";
 import { MaterialComponent } from "./Screens/material-component/material-component";
-import { UsersListComponent } from "./Components/user-list/user-list";
+import { UsersListComponent } from "./Screens/user-list/user-list";
+import { NotFoundComponent } from "./Screens/not-found-component/not-found-component"; 
+import { UsersHomeComponent } from "./Screens/users-home-component/users-home-component";
 
 export const routes: Routes = [
-  { path: 'forms', component: FormsComponent },
+  { path: 'forms',    component: FormsComponent },
   { path: 'material', component: MaterialComponent },
-  { path: 'users', component: UsersListComponent }
+  { 
+    path: 'users',    component: UsersHomeComponent,
+    children: [
+      { path: 'list', component: UsersListComponent}
+    ] 
+  },
+  { path: '**',       component: NotFoundComponent},
 ];
