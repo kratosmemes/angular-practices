b12894d5770ce1062d66f6f2f679147a8827461c
Angel <angelpadillasanchez@hotmail.com>
2025-09-03 00:17:04 -0600

    Se agrega empty ScreenComponent

---
+++ src/app/Screens/empty-screen/empty-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-screen',
+  imports: [],
+  templateUrl: './empty-screen.html',
+  styleUrl: './empty-screen.scss'
+})
+export class EmptyScreenComponent {
+
+}
