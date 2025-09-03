151f49996cb3358c1d3f1049705b4e9ba81de200
Angel <angelpadillasanchez@hotmail.com>
2025-09-03 00:18:25 -0600

    Se agrega EmptyScreen a routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,10 @@ import { UsersListComponent } from "./Screens/user-list/user-list";
 import { NotFoundComponent } from "./Screens/not-found-component/not-found-component"; 
 import { UsersHomeComponent } from "./Screens/users-home-component/users-home-component";
 import { ReactiveFormsComponent } from "./Screens/reactive-forms-component/reactive-forms-component";
+import { EmptyScreenComponent } from "./Screens/empty-screen/empty-screen";
 
 export const routes: Routes = [
+  { path: '', component: EmptyScreenComponent },
   { path: 'forms',    component: FormsComponent },
   { path: 'material', component: MaterialComponent },
   { path: 'reactiveForms', component: ReactiveFormsComponent },
